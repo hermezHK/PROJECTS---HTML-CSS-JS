3e9b7bbae1f36134e150e020fd4f9dc4ae191d02
Hermez Jaramillo <angelo_stk06@hotmail.com>
2024-07-10 20:08:51 -0500

    feat : added coments

--- GHOST-ANIMATION/index.js
+++ GHOST-ANIMATION/index.js
@@ -1,5 +1,7 @@
+// obtain the body
 const bodyEl = document.querySelector("body")
 
+//mousemove event
 bodyEl.addEventListener("mousemove", (event) => {
     const xPos = event.offsetX;
     const yPos = event.offsetY;
@@ -8,10 +10,12 @@ bodyEl.addEventListener("mousemove", (event) => {
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
 
+    //different icon sizes
     const size = Math.random() * 100;
     spanEl.style.width = size + "px";
     spanEl.style.height = size + "px";
 
+    //set time
     bodyEl.appendChild(spanEl)
     setTimeout(() => {
         spanEl.remove();
